98f28f828db934b96c377bffd83f2cba5871eb72
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2022-03-24 18:21:09 -0600

    Final Version

--- Fetch_JSON/app/src/main/java/com/example/fetch_json_excercise/MainActivity.java
+++ Fetch_JSON/app/src/main/java/com/example/fetch_json_excercise/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adaptery);
     }
 
-    
+
     public class GetData extends AsyncTask<Integer, Integer, String> {
 
 
